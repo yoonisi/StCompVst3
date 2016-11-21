50d4b983f95d7d01a764c97ccdf5ca8958332f0f
yoonisi <oonisi@gmail.com>
2016-11-21 21:08:30 +0900

    add project files

---
+++ StCompVst3/Version.cpp
@@ -0,0 +1,13 @@
+
+namespace KBVstPlugIn {
+	class StCompVersion {
+	public:
+		static int GetVersionNumber() {
+			return 0;
+		}
+
+	private:
+		StCompVersion(){}
+		StCompVersion(const StCompVersion& c){}
+	};
+}
